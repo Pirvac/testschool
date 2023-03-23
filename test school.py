9060b06cfed341ed080e30805588ae96c76861c0
Pirvac <127548984+Pirvac@users.noreply.github.com>
2023-03-23 16:35:32 +0100

    Add files via upload

--- test school.py
+++ test school.py
@@ -1,21 +1,37 @@
-class Person:
+from abc import ABC, abstractmethod
+
+class Person(ABC):
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        
-class Teacher(Person):
-    pass
 
-teacher_01 = Person("anass", 30)
-teacher_02 = Person("jamal", 39)
-teacher_03 = Person("khalid", 41)
-teacher_04 = Person("adil", 42) 
+    def show_info(self,name,age):
+        return f"the person is called  {self.name} and he is {self.age} years old"
+
+      
+          
+
+class Teacher(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+    
+
+          
+
 
 class Student(Person):
-    pass
-        
-student_01 = Person("John", 20)
-student_02 = Person("Kate", 19)
-student_03 = Person("Tom", 21)
-student_04 = Person("Lucy", 22)
+    def __init__(self, name, age):
+        super().__init__(name, age)
     
+    def show_info(self):
+
+
+
+teacher01 = Teacher("Adam", 39)
+student01 = Student("Moad", 28)
+
+show_info(teacher01)
+show_info(student01)
+
+        
+
