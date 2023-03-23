ae4e89dd41c7462898f79068fc6ca7237944913e
Pirvac <127548984+Pirvac@users.noreply.github.com>
2023-03-23 16:01:36 +0100

    Add files via upload

---
+++ test school.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+class Teacher(Person):
+    pass
+
+teacher_01 = Person("anass", 30)
+teacher_02 = Person("jamal", 39)
+teacher_03 = Person("khalid", 41)
+teacher_04 = Person("adil", 42) 
+
+class Student(Person):
+    pass
+        
+student_01 = Person("John", 20)
+student_02 = Person("Kate", 19)
+student_03 = Person("Tom", 21)
+student_04 = Person("Lucy", 22)
+    
